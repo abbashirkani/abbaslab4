af6392bcbca30854f71374f2aba9c12848fe9f80
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 23:38:19 +0530

    Create abbaslab4q4.cpp

---
+++ abbaslab4q4.cpp
@@ -0,0 +1,23 @@
+# abbaslab4
+// q 4
+
+
+#include<iostream>
+using namespace std;
+void even_odd()
+{
+ int a;
+ cout<<"Enter a number : ";
+ cin>>a;
+ if(a%2==0)
+ {
+  cout<<a<<" is even"<<endl;
+ }
+ else
+  cout<<a<<" is odd.";
+}
+int main()
+{
+ even_odd();
+ return 0;
+}
